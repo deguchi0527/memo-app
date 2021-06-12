1ba4001e625b2a6a3dc0325616f35a67f4237a55
deguchi0527 <ryodegu0527@gmail.com>
2021-06-12 16:58:42 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'homes', to: 'homes#index'
 
   namespace :api, format: 'json' do
-    resources :memos, only: [:index]
+    resources :memos, only: [:index, :create]
   end
 end
